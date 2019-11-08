abb78c4b523d1483df91b24dccb10c84844bea2e
Junwei Feng <fjw9008@gmail.com>
2019-11-08 13:12:17 +0100

    commit

--- maciek/benchmarks2/vector_visit.cc
+++ maciek/benchmarks2/vector_visit.cc
@@ -7,7 +7,8 @@
 #include <random>
 #include <vector>
 
-std::vector<int> generateRandomVector(int size) {
+std::vector<int> generateRandomVector(int size)
+{
   static std::default_random_engine rd;
   static std::uniform_int_distribution<int> dist(0, size - 1);
 
@@ -18,53 +19,59 @@ std::vector<int> generateRandomVector(int size) {
   return v;
 }
 
-struct InOrder {
-  static void init(std::vector<int> &v2) {
+struct InOrder
+{
+  static void init(std::vector<int> &v2)
+  {
     for (int i = 0; i < v2.size(); ++i)
       v2[i] = i;
   }
 };
 
-struct Shuffled {
-  static void init(std::vector<int> &v2) {
+struct Shuffled
+{
+  static void init(std::vector<int> &v2)
+  {
     for (int i = 0; i < v2.size(); ++i)
       v2[i] = i;
     std::random_shuffle(v2.begin(), v2.end());
   }
 };
 
-template <typename Initializer> static void visit(benchmark::State &state) {
+template <typename Initializer>
+static void visit(benchmark::State &state)
+{
   int size = state.range(0);
   std::vector<int> v1 = generateRandomVector(size);
   std::vector<int> v2;
   v2.resize(size);
   Initializer::init(v2);
 
-  papi::event_set<PAPI_TOT_INS, PAPI_TOT_CYC> events;
-  events.start_counters();
+  //papi::event_set<PAPI_TOT_INS, PAPI_TOT_CYC> events;
+  //events.start_counters();
 
-  for (auto _ : state) {
+  for (auto _ : state)
+  {
     int sum = 0;
     for (int i : v2)
       sum += v1[i];
     benchmark::DoNotOptimize(&sum);
   }
 
-  events.stop_counters();
+  //events.stop_counters();
 
-  double ipc = double(events.get<PAPI_TOT_INS>().counter()) /
-               events.get<PAPI_TOT_CYC>().counter();
-  state.counters["ipc"] = ipc;
+  //state.counters["ipc"] = double(events.get<PAPI_TOT_INS>().counter()) /
+  //                        events.get<PAPI_TOT_CYC>().counter();
   state.SetBytesProcessed(2 * size * sizeof(int) * state.iterations());
   state.counters["data"] = 2 * size * sizeof(int);
 }
 
 BENCHMARK_TEMPLATE(visit, InOrder)
-    ->RangeMultiplier(2)
+    ->RangeMultiplier(32)
     ->Range(1024, 1024 * 1024 * 4);
 
 BENCHMARK_TEMPLATE(visit, Shuffled)
-    ->RangeMultiplier(2)
+    ->RangeMultiplier(32)
     ->Range(1024, 1024 * 1024 * 4);
 
 BENCHMARK_MAIN();
